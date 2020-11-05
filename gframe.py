0de97b88c7b731d815d1b6dac25d6e4d12172375
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-05 08:50:33 -0600

    Update gframe.py

--- gframe.py
+++ gframe.py
@@ -10,6 +10,6 @@ def init():
   except:
     raise SystemError("GFRAME is undefined, unknown game template.")
   if foo not in templates:
-    raise SystemError("GFRAME is undefined, unknown game template.")
+    raise SystemError("GFRAME is unregistered, unknown game template.")
   if foo == "textadventure":
     inittext()
